0b044a3009f49f39ae4c3e235c7374dbad8c75ea
Yasmin Nabila Othman <yasminnabila0502@gmail.com>
2025-05-14 22:59:38 +0800

    final touches

---
+++ config.js
@@ -0,0 +1,3 @@
+const CONFIG = {
+    FONT_LINK: "https://kit.fontawesome.com/7ac8b333e9.js"
+}
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const faScript = document.createElement("script");
+faScript.src = CONFIG.FONT_LINK;
+faScript.crossOrigin = "anonymous";
+document.head.appendChild(faScript);
+
 const hamburger = document.querySelector('.hamburger');
 const nav_section = document.querySelector('.nav_section');
 
@@ -5,3 +10,11 @@ hamburger.addEventListener('click', () => {
     nav_section.classList.toggle('nav--open');
     hamburger.classList.toggle('hamburger--open');
 });
+
+document.querySelectorAll('.view-project').forEach(button =>{
+   if (button.textContent.trim().toLowerCase() === 'in progress') {
+      button.style.backgroundColor = '#FFD700'; 
+      button.style.color = '#000'; 
+      button.style.cursor = 'not-allowed';
+    }
+})
